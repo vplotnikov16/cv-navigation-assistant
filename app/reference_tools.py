ca326dfd95f3d909c08b996ed97087df5084b5cf
vplotnikov16 <vplotnikov16@yandex.ru>
2025-11-16 08:21:49 +0500

    исправление распознавания

--- app/reference_tools.py
+++ app/reference_tools.py
@@ -169,7 +169,10 @@ def bytes_to_cv2_image(image_bytes: bytes) -> np.ndarray:
 def inference(image_bytes: bytes, model_type: Models = Models.Objects) -> ultralytics.engine.results.Results:
     model = get_model(model_type)
     cv2_image = bytes_to_cv2_image(image_bytes)
-    detections = model.predict(cv2_image, verbose=False, conf=0.65)[0]
+    if model_type == Models.Accessibility:
+        detections = model.predict(cv2_image, verbose=False, conf=0.65)[0]
+    else:
+        detections = model.predict(cv2_image, verbose=False)[0]
     return detections
 
 
